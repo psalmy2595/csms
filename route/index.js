4d45d482800c7aed5220fa9e41632bc973391347
Tunde Nasri <tundenasri@gmail.com>
2021-03-12 19:53:35 +0100

    setup message sending api

--- route/index.js
+++ route/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const { auth } = require('../middleware/auth');
-const { User } = require('../models/user');
-
+const express = require("express");
+const { auth } = require("../middleware/auth");
+const { User } = require("../models/user");
+const axios = require("axios");
 
 const router = express.Router();
 
@@ -39,29 +39,75 @@ router.post("/register", function (req, res) {
 
 router.post("/login", function (req, res) {
   User.findOne({ email: req.body.email }, (err, user) => {
-    if(err) return res.status(400).json(err)
-  
-      user.comparePassword(req.body.password, (err, isMatch) => {
-        if (!isMatch) return res.status(400).send("Error", err);
-        // // Generate token on login
-        user.generateToken((err, tok) => {
-          if (err) return res.status(400).send(err);
-          res.cookie("w_authExp", tok.tokenExp);
-          res.cookie("w_auth", tok.token);
-          console.log("success");
-          res.redirect("dashboard");
-        });
-      });
+    if (err) return res.status(400).json(err);
 
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.status(400).send("Error", err);
+      // // Generate token on login
+      user.generateToken((err, tok) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("w_authExp", tok.tokenExp);
+        res.cookie("w_auth", tok.token);
+        console.log("success");
+        res.redirect("dashboard");
+      });
+    });
   });
 });
 
-router.get('/logout', function(req, res) {
-  res.clearCookie('w_auth');
-  res.clearCookie('w_authExp');
+router.post("/dashboard", function (req, res) {
+  const { message, sender, recipient } = req.body
+  console.log("🚀 ~ file: index.js ~ line 61 ~ req.body", req.body);
+  // send the message here, when you get a response, show the response;
 
-  res.redirect('/login');
-})
+  const options = {
+    data: {
+      SMS: {
+        auth: {
+          username: "olatunjiyakub99@gmail.com",
+          apikey: "f338d58a940ea765f2c8c35a44e156b7540fb8fd",
+        },
+        message: {
+          sender: sender,
+          messagetext: message,
+          flash: "0",
+        },
+        recipients: {
+          gsm: [
+            {
+              msidn: recipient,
+            },
+          ],
+        },
+      },
+    },
+    method: "POST",
+    url: "http://api.ebulksms.com:8080/sendsms.json",
+    headers: { "content-type": "application/json" },
+  };
 
+  axios(options)
+    .then((res) => {
+      const status = res.data.response.status;
+
+      if (status === 'SUCCESS') {
+        console.log('success')
+      } else {
+        throw new Error(`Unable to send message - Reason: ${status}`)
+      }
+    })
+    .catch((error) => {
+      console.log("🚀 ~ file: index.js ~ line 93 ~ axios ~ error", error.message);
+    });
+
+    res.redirect('/dashboard');
+});
+
+router.get("/logout", function (req, res) {
+  res.clearCookie("w_auth");
+  res.clearCookie("w_authExp");
+
+  res.redirect("/login");
+});
 
 module.exports = router;
