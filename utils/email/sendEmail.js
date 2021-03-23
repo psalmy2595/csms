d0b3e61bc918ba34347fa97a9abc8f2896e1446c
Tunde Nasri <tundenasri@gmail.com>
2021-03-23 02:38:40 +0100

    log email transporter connection status

--- utils/email/sendEmail.js
+++ utils/email/sendEmail.js
@@ -3,18 +3,21 @@ const handlebars = require("handlebars");
 const fs = require("fs");
 const path = require("path");
 
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+transporter.verify().then(() => console.log('Connected to email server'))
+  .catch(() => console.log('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
+  
 const sendEmail = async (email, subject, payload, template) => {
   try {
     // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
     const source = fs.readFileSync(path.join(__dirname, template), "utf8");
     const compiledTemplate = handlebars.compile(source);
     const options = () => {
